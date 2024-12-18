6c361ff2cb59a9816b722a7a93fc5e20ca2a2b54
Suffer Engineer <57523413+hubilya@users.noreply.github.com>
2024-12-18 21:34:05 +0200

    Add files via upload

---
+++ second-script.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+        var x = document.getElementById("myNav");
+            if (x.className === "nav") {
+            x.className += " responsive";
+            } 
+            else {
+                x.className = "nav";
+            }
+     }
